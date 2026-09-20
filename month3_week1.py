f630f12b5b28393c25fd9773191300c76f2ded96
Amir Masoud <amdeyhim@gmail.com>
2026-09-20 16:03:09 +0330

    Complete inventory checker exercise

---
+++ month3_week1.py
@@ -0,0 +1,79 @@
+import pandas as pd
+
+level = ["سشف", "low", "medium"]
+
+products = ["product A", "product B", "product C"]
+
+level_products = pd.Series(data=level, index=products)
+
+print(level_products)
+print(level_products["product A"])
+
+#####################################################
+import pandas as pd
+
+viewer = [500, 600, 450]
+days = ["sat", "sun", "mon"]
+viewer_days = pd.Series(data=viewer, index=days)
+print(viewer_days)
+print(viewer_days["mon"])
+
+#####################################################
+
+import pandas as pd
+
+sales_data = {
+    "Product": ["Product A", "Product B", "Product C"],
+    "Revenue": [1200, 800, 1500],
+    "Status": ["High", "Low", "High"],
+    "visitors": [5, 8, 7],
+}
+df = pd.DataFrame(data=sales_data)
+
+print(df)
+
+#####################################################
+
+import pandas as pd
+
+shopping_data = {
+    "user_ID": [101, 102, 103],
+    "category": ["electronics", "clothing", "book"],
+    "cart_value": [4500, 1200, 300],
+}
+df = pd.DataFrame(data=shopping_data)
+print(df)
+
+####################################################
+
+import pandas as pd
+
+data = {
+    "user_ID": [101, 102, 103],
+    "category": ["electronics", "clothing", "book"],
+    "cart_value": [4500, 1200, 300],
+}
+df = pd.DataFrame(data)
+
+df.to_csv("user_carts.csv", index=False)
+
+loaded_df = pd.read_csv("user_carts.csv")
+
+print(loaded_df)
+
+####################################################
+
+import pandas as pd
+
+data = {
+    "user_ID": [1001, 1002, 1003],
+    "rating": [4, 5, 2],
+    "platform": ["web", "ios", "android"],
+}
+df = pd.DataFrame(data)
+
+df.to_csv("user_feedbacks.csv", index=False)
+
+loaded_df = pd.read_csv("user_feedbacks.csv")
+
+print(loaded_df)
